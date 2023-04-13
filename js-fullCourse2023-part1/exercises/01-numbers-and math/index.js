658bcbdd76c6abf9a75bdf673e4e049940a78080
halidduman <halidduman60@gmail.com>
2023-04-13 14:56:16 +0300

    Numbers and Math

---
+++ js-fullCourse2023-part1/exercises/01-numbers-and math/index.js
@@ -0,0 +1,22 @@
+//  alistirma
+
+
+// 1. Bir restoranda 1 çorbayı 10$'a, 3 hamburgeri tanesi 8$'a ve 1 dondurmayı 5$'a
+// sipariş ediyorsunuz. Siparişin maliyetini hesaplayın
+
+console.log (10 + 3 * 8 + 5)
+
+// 2. 2 arkadaşınız (toplam 3 kişi) ile bir restorandasınız ve 2a ile aynı siparişi
+// veriyorsunuz. Her bir kişinin ne kadar ödediğini hesaplayın.
+
+console.log ((10 + 3 * 8 + 5) /3)
+
+// 2c. Bir tost makinesinin (18,50$) ve 2 gömleğin (her biri 7,50$) toplam maliyetini hesaplayın.
+// 2d. Alıştırma 2c'deki toplam için %10'luk bir vergi hesaplayın.
+
+console.log (18.5 + 2* 7.5)
+
+// 3. 2c'deki toplam için %20'lik bir vergi hesaplayın (%1 = 1 / 100, dolayısıyla %20 = 20 /
+// 100 = 0,2 olduğunu unutmayın)
+
+console.log ((18.5 + 2* 7.5)* 0.1)
